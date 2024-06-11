9bde74e3122a11198dcd3d318a89d60c505f5125
Omar Lengua <omarns21@gmail.com>
2024-06-11 15:26:48 +0200

    agregado avance

---
+++ cursoProgramacion/mokepon/js/codeOfMokepon.js
@@ -0,0 +1,50 @@
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+}
+
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+    let spanMascotaJugador = document.getElementById('mascota-jugador')
+    
+    if (inputHipodoge.checked) {
+        spanMascotaJugador.innerHTML = 'Hipodoge'
+    } else if (inputCapipepo.checked) {
+        spanMascotaJugador.innerHTML = 'Capipepo'
+    } else if (inputRatigueya.checked) {
+        spanMascotaJugador.innerHTML = 'Ratigueya'
+    } else {
+        alert("Selecciona una Mascota")
+    }
+    seleccionarMascotaEnemigo()
+}
+
+function seleccionarMascotaEnemigo() {
+    let ataqueAleatorio = aleatorio(1, 3)
+    let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
+    let pronombre = document.getElementById('pronombre-mascotaene')
+
+    if (ataqueAleatorio == 1) {
+        spanMascotaEnemigo.innerHTML = 'Hipodoge'
+        pronombre.innerHTML = 'El'
+    } else if (ataqueAleatorio == 2) {
+        spanMascotaEnemigo.innerHTML = 'Capipepo'
+        pronombre.innerHTML = 'El'
+    } else if (ataqueAleatorio == 3) {
+        spanMascotaEnemigo.innerHTML = 'Ratigueya'
+        pronombre.innerHTML = 'La'
+    } else {
+        alert("Ataque fallido")
+    }
+
+}
+
+function aleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+
+window.addEventListener('load', iniciarJuego)
